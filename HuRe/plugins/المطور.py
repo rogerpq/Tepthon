0fb6448fad1e7200baefc8ee2599702856742540
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-05-05 00:57:28 +0300

    Update المطور.py

--- HuRe/plugins/المطور.py
+++ HuRe/plugins/المطور.py
@@ -39,7 +39,7 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = " https://telegra.ph/file/83d3c9610bc957b20e35a.jpg "
+    CAT_IMG = " https://telegra.ph/file/a99ffc2e6b7c8a24ffdcb.jpg "
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
